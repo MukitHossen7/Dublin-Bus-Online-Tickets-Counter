1b1c573511cffe9977cc26b0ecdd56e9d8be1c28
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-21 19:50:28 +0600

    alert massage show

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menu-btn");
 const mobileMenuEl = document.getElementById("mobileMenu");
 const defaultText = document.getElementById("default-text");
 const totalBookedSeat = document.getElementById("total-booked");
+const availableSeat = document.getElementById("available-seat");
 // const showSeat = document.getElementById('default-text');
 
 const collectSeat = [];
@@ -13,11 +14,14 @@ function seatSelectBtn(event) {
   //     <span>550</span>
   //     </div>
   //   `;
-
+  if (collectSeat.length >= 4) {
+    alert("Maximum seat added");
+    return;
+  }
   event.classList.add("bg-green-400");
   event.classList.add("text-white");
   collectSeat.push(event.innerText).length;
   totalBookedSeat.innerHTML = collectSeat.length;
-  console.log(collectSeat);
-  console.log(collectSeat.length);
+  const availableSeatValue = parseFloat(availableSeat.innerText);
+  availableSeat.innerText = availableSeatValue - 1;
 }
